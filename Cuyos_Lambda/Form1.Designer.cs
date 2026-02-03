b2c0a87200b1df89b189c3d6f6f195753dd6a709
Aian Cuyos <aiarn1006@gmail.com>
2026-02-03 14:00:36 +0800

    Add files via upload

---
+++ Cuyos_Lambda/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace Cuyos_Lambda
+{
+    partial class frmCafeteria
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtQuantity = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.rdbDinein = new System.Windows.Forms.RadioButton();
+            this.rdbTakeout = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtQuantity
+            // 
+            this.txtQuantity.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtQuantity.Location = new System.Drawing.Point(347, 261);
+            this.txtQuantity.Name = "txtQuantity";
+            this.txtQuantity.Size = new System.Drawing.Size(297, 35);
+            this.txtQuantity.TabIndex = 0;
+            this.txtQuantity.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtQuantity_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(347, 243);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Enter Quantity:";
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.BackColor = System.Drawing.Color.Transparent;
+            this.btnCheck.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCheck.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCheck.ForeColor = System.Drawing.Color.White;
+            this.btnCheck.Location = new System.Drawing.Point(534, 438);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(141, 44);
+            this.btnCheck.TabIndex = 2;
+            this.btnCheck.Text = "Check Out";
+            this.btnCheck.UseVisualStyleBackColor = false;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
+            // rdbDinein
+            // 
+            this.rdbDinein.AutoSize = true;
+            this.rdbDinein.BackColor = System.Drawing.Color.Transparent;
+            this.rdbDinein.Font = new System.Drawing.Font("Bahnschrift", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdbDinein.Location = new System.Drawing.Point(374, 339);
+            this.rdbDinein.Name = "rdbDinein";
+            this.rdbDinein.Size = new System.Drawing.Size(87, 27);
+            this.rdbDinein.TabIndex = 3;
+            this.rdbDinein.TabStop = true;
+            this.rdbDinein.Text = "Dine In";
+            this.rdbDinein.UseVisualStyleBackColor = false;
+            // 
+            // rdbTakeout
+            // 
+            this.rdbTakeout.AutoSize = true;
+            this.rdbTakeout.BackColor = System.Drawing.Color.Transparent;
+            this.rdbTakeout.Font = new System.Drawing.Font("Bahnschrift", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdbTakeout.Location = new System.Drawing.Point(528, 339);
+            this.rdbTakeout.Name = "rdbTakeout";
+            this.rdbTakeout.Size = new System.Drawing.Size(100, 27);
+            this.rdbTakeout.TabIndex = 4;
+            this.rdbTakeout.TabStop = true;
+            this.rdbTakeout.Text = "Take Out";
+            this.rdbTakeout.UseVisualStyleBackColor = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(607, 294);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Clear";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // frmCafeteria
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Cuyos_Lambda.Properties.Resources.aw;
+            this.ClientSize = new System.Drawing.Size(708, 499);
+            this.Controls.Add(this.rdbTakeout);
+            this.Controls.Add(this.rdbDinein);
+            this.Controls.Add(this.btnCheck);
+            this.Controls.Add(this.txtQuantity);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmCafeteria";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cafeteria";
+            this.Load += new System.EventHandler(this.frmCafeteria_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtQuantity;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.RadioButton rdbDinein;
+        private System.Windows.Forms.RadioButton rdbTakeout;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Cuyos_Lambda/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cuyos_Lambda
+{
+    public partial class frmCafeteria : Form
+    {
+        public frmCafeteria()
+        {
+            InitializeComponent();
+
+            // Lambda expression for button click
+            btnCheck.Click += (sender, e) =>
+            {
+                int quantity;
+                string orderType = "";
+
+                // Determine order type
+                if (rdbDinein.Checked)
+                    orderType = "Dine In";
+                else if (rdbTakeout.Checked)
+                    orderType = "Take Out";
+                else
+                    orderType = "No order type selected";
+
+                if (int.TryParse(txtQuantity.Text, out quantity))
+                {
+                    if (quantity > 0)
+                    {
+                        MessageBox.Show(
+                            $"Order Accepted\nOrder Type: {orderType}\nQuantity: {quantity}", "Order Status", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Invalid Order", "Order Status", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Invalid Order", "Order Status", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            };
+        }
+
+        private void frmCafeteria_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            txtQuantity.Clear();
+           
+        }
+
+        private void txtQuantity_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Allow digits and backspace only
+            if (!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
